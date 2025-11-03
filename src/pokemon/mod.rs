cf2b40f797afa23214b871b4f6008b124a7a3867
Alex Haslehurst <alex.haslehurst@gmail.com>
2025-11-03 01:29:09 +0000

    initial map routes

--- src/pokemon/mod.rs
+++ src/pokemon/mod.rs
@@ -1,16 +1,17 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fmt::{Display, Formatter};
 use std::ops::{Deref, DerefMut, Index, IndexMut};
 use badge::Badge;
 use map::Map;
 use species::PokemonSpecies;
 use unicode_segmentation::UnicodeSegmentation;
+use party::PokemonParty;
 use crate::game_boy::GameBoy;
 use crate::geometry::Point8;
 use crate::mmu::MMU;
 use crate::pokemon::move_name::{PokemonMove, PokemonMoveName};
 use crate::pokemon::pokemon::{Pokemon, PokemonStats, PokemonType};
-use crate::pokemon::sprite::{PictureId, Sprite                          };
+use crate::pokemon::sprite::{PictureId, Sprite};
 
 pub mod badge;
 pub mod map;
@@ -18,7 +19,8 @@ pub mod pokemon;
 pub mod status;
 pub mod species;
 pub mod move_name;
-mod sprite;
+pub mod sprite;
+pub mod party;
 
 #[derive(Debug)]
 pub struct PokemonApi<'a> {
@@ -73,13 +75,19 @@ impl<'a> PokemonApi<'a> {
     pub fn map_state(&self) -> Result<MapState, String> {
         let mmu = self.mmu();
 
-        let map_parser = MapParser::from_mmu(mmu)?;
-        println!("{}", map_parser);
+        let position = Point8 { x: mmu.read(0xD362), y: mmu.read(0xD361) };
+        let map = CurrentMap::from_mmu(mmu)?;
+        let meta_tile_map = MetaTileMap::new(&map);
+        println!("{}", meta_tile_map);
+        let routes = meta_tile_map.routes(position);
+        for route in routes.into_iter() {
+            println!("{:?}", route);
+        }
 
         Ok(MapState {
-            map: map_parser.map,
-            position: Point8 { x: mmu.read(0xD362), y: mmu.read(0xD361) },
-            sprites: mmu.read_sprites()?
+            map: map.map,
+            position,
+            sprites: map.sprites
         })
     }
 }
@@ -93,53 +101,18 @@ pub struct PlayerState {
     pub money: u32,
 }
 
-#[derive(Debug, Clone, Eq, PartialEq, Default)]
-pub struct PokemonParty(Vec<Pokemon>);
-
-impl PokemonParty {
-    pub fn push(&mut self, pokemon: Pokemon) -> Result<(), String> {
-        if self.0.len() >= PokemonBlockAddresses::PARTY_MAX as usize {
-            Err("Party is full".to_string())
-        } else {
-            self.0.push(pokemon);
-            Ok(())
-        }
-    }
-
-    pub fn len(&self) -> usize {
-        self.0.len()
-    }
-}
-
-impl Index<usize> for PokemonParty {
-    type Output = Pokemon;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        self.0.index(index)
-    }
-}
-
-impl IndexMut<usize> for PokemonParty {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        self.0.index_mut(index)
-    }
-}
-
-impl IntoIterator for PokemonParty {
-    type Item = Pokemon;
-    type IntoIter = std::vec::IntoIter<Pokemon>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
-    }
-}
-
 
 #[derive(Debug, Clone)]
 pub struct MapState {
     pub map: Map,
     pub position: Point8,
     pub sprites: Vec<Sprite>
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct WarpEvent {
+    pub position: Point8,
+    pub map_id: Map,
 }
 
 trait PokemonEncoding {
@@ -152,6 +125,8 @@ trait PokemonEncoding {
     fn write_pokemon(&mut self, base_address: u16, index: u16, pokemon: &Pokemon);
 
     fn read_sprites(&self) -> Result<Vec<Sprite>, String>;
+
+    fn read_warp_events(&self) -> Result<Vec<WarpEvent>, String>;
 }
 
 impl PokemonEncoding for MMU {
@@ -403,18 +378,51 @@ impl PokemonEncoding for MMU {
         }
         Ok(sprites)
     }
+
+    fn read_warp_events(&self) -> Result<Vec<WarpEvent>, String> {
+        let warp_count = self.read(0xD3AE) as u16;
+        let mut result = vec![];
+        let last_map_id = self.read(0xD73C);
+        for index in 0..warp_count {
+            let address = 0xD3AF + index * 4;
+            let map_id = self.read(address + 3);
+            let warp = WarpEvent {
+                position: Point8 { y: self.read(address), x: self.read(address + 1) },
+                // warp_id: self.read(address + 2),
+                map_id: Map::from_repr(if map_id == 0xFF {
+                    last_map_id
+                } else {
+                    map_id
+                }).ok_or_else(|| format!("Invalid map number {}", map_id))?,
+            };
+            result.push(warp);
+        }
+        Ok(result)
+    }
 }
 
-pub struct MapParser {
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum MetaTile {
+    Empty,
+    Obstacle,
+    Sprite(Sprite),
+    Warp(Map),
+    // TODO map connection
+    // TODO signs
+}
+
+pub struct CurrentMap {
     map: Map,
     map_width_blocks: usize,
     map_height_blocks: usize,
     map_data: Vec<u8>,
     block_data: Vec<u8>,
     collision_tiles: HashSet<u8>,
+    warp_events: Vec<WarpEvent>,
+    sprites: Vec<Sprite>,
 }
 
-impl MapParser {
+impl CurrentMap {
 
     pub fn from_mmu(mmu: &MMU) -> Result<Self, String> {
         // any rom data we read must be directly from the rom banks as the game is not guaranteed to have the correct bank loaded
@@ -442,6 +450,10 @@ impl MapParser {
         let block_data_address = mmu.read_u16_le(0xD52C);
         let block_data = mmu.rom_data_from_pointer(tileset_bank, block_data_address, (max_block_id + 1) * Self::BLOCK_TILES).to_vec();
 
+        let warp_events = mmu.read_warp_events()?;
+
+        let sprites = mmu.read_sprites()?;
+
         Ok(Self {
             map,
             map_width_blocks,
@@ -449,55 +461,272 @@ impl MapParser {
             map_data,
             block_data,
             collision_tiles,
+            warp_events,
+            sprites,
         })
     }
 
     pub const BLOCK_TILE_WIDTH: usize = 4; // a block is 4x4 tiles
     pub const BLOCK_TILES: usize = Self::BLOCK_TILE_WIDTH * Self::BLOCK_TILE_WIDTH;
-    pub const TILES_PER_POSITION: usize = 2; // a position on the map is 2x2 tiles
+    pub const TILES_PER_META: usize = 2; // a meta tile on the map is 2x2 graphical tiles
 
-    pub fn can_walk_over_tile(&self, x: usize, y: usize) -> bool {
-        let block_index = self.map_data[(x / Self::BLOCK_TILE_WIDTH) + (y / Self::BLOCK_TILE_WIDTH) * self.map_width_blocks] as usize;
+    pub fn is_empty(&self, tile_x: usize, tile_y: usize) -> bool {
+        let block_x = tile_x / Self::BLOCK_TILE_WIDTH;
+        let block_y = tile_y / Self::BLOCK_TILE_WIDTH;
+        let block_index = self.map_data[block_x + block_y * self.map_width_blocks] as usize;
         let block_offset = block_index * Self::BLOCK_TILES;
-        let tile_offset = (x % Self::BLOCK_TILE_WIDTH) + (y % Self::BLOCK_TILE_WIDTH) * Self::BLOCK_TILE_WIDTH;
+        let tile_offset = (tile_x % Self::BLOCK_TILE_WIDTH) + (tile_y % Self::BLOCK_TILE_WIDTH) * Self::BLOCK_TILE_WIDTH;
         let tile_index = self.block_data[block_offset + tile_offset];
         self.collision_tiles.contains(&tile_index)
     }
 
-    pub fn navigable_map(&self) -> Vec<bool> {
-        let width = self.map_width_blocks * Self::TILES_PER_POSITION;
-        let height = self.map_height_blocks * Self::TILES_PER_POSITION;
-        let mut result = vec![false; width * height];
+    pub fn meta_width(&self) -> usize {
+        self.map_width_blocks * Self::TILES_PER_META
+    }
+
+    pub fn meta_height(&self) -> usize {
+        self.map_height_blocks * Self::TILES_PER_META
+    }
+
+    pub fn meta_tiles(&self) -> Vec<MetaTile> {
+        let width = self.meta_width();
+        let height = self.meta_height();
+
+        // start off assuming all tiles are obstacles
+        let mut result = vec![MetaTile::Obstacle; width * height];
 
         let width_tiles = self.map_width_blocks * Self::BLOCK_TILE_WIDTH;
         let height_tiles = self.map_height_blocks * Self::BLOCK_TILE_WIDTH;
         for tile_y in 0..height_tiles {
-            let y = tile_y / Self::TILES_PER_POSITION;
+            let y = tile_y / Self::TILES_PER_META;
             for tile_x in 0..width_tiles {
-                let x = tile_x / Self::TILES_PER_POSITION;
+                let x = tile_x / Self::TILES_PER_META;
                 let index = x + y * width;
-                // if you can walk over any tile in a map position (2x2 tiles) then you can walk over the whole position
-                if !result[index] && self.can_walk_over_tile(tile_x, tile_y) {
-                    result[index] = true;
+                // if you can walk over any tile in a map position (2x2 tiles) then you can walk over the whole meta tile
+                if result[index] == MetaTile::Obstacle && self.is_empty(tile_x, tile_y) {
+                    result[index] = MetaTile::Empty;
                 }
             }
+        }
+
+        // now check for sprites
+        for sprite in self.sprites.iter().filter(|sprite| !sprite.hidden) {
+            let index = sprite.position.x as usize + sprite.position.y as usize * width;
+            result[index] = MetaTile::Sprite(*sprite);
+        }
+
+        // now check for warp events
+        for warp_event in &self.warp_events {
+            let index = warp_event.position.x as usize + warp_event.position.y as usize * width;
+            result[index] = MetaTile::Warp(warp_event.map_id);
         }
 
         result
     }
 }
 
-impl Display for MapParser {
+pub struct MetaTileMap {
+    width: usize,
+    height: usize,
+    meta_tiles: Vec<MetaTile>,
+    sprites: Vec<Sprite>,
+    warp_targets: HashSet<Map>,
+}
+
+impl MetaTileMap {
+    pub fn new(map: &CurrentMap) -> Self {
+        let meta_tiles = map.meta_tiles();
+        let width = map.meta_width();
+        let height = map.meta_height();
+        let sprites = map.sprites.clone();
+        let warp_targets = map.warp_events.iter()
+            .map(|warp_event| warp_event.map_id)
+            .collect();
+        Self { width, height, meta_tiles, sprites, warp_targets }
+    }
+
+    pub fn route_between(&self, from: Point8, to: Point8) -> Option<Route> {
+        use std::collections::{BinaryHeap, HashMap};
+        use std::cmp::Ordering;
+
+        #[derive(Clone, Eq, PartialEq)]
+        struct Node {
+            position: Point8,
+            cost: u32,
+            heuristic: u32,
+        }
+
+        impl Ord for Node {
+            fn cmp(&self, other: &Self) -> Ordering {
+                (other.cost + other.heuristic).cmp(&(self.cost + self.heuristic))
+            }
+        }
+
+        impl PartialOrd for Node {
+            fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+                Some(self.cmp(other))
+            }
+        }
+
+        let heuristic = |pos: Point8| -> u32 {
+            (pos.x.abs_diff(to.x) + pos.y.abs_diff(to.y)) as u32
+        };
+
+        let mut open_set = BinaryHeap::new();
+        let mut came_from: HashMap<Point8, (Point8, Direction)> = HashMap::new();
+        let mut g_score: HashMap<Point8, u32> = HashMap::new();
+
+        open_set.push(Node { position: from, cost: 0, heuristic: heuristic(from) });
+        g_score.insert(from, 0);
+
+        while let Some(current) = open_set.pop() {
+            if current.position == to {
+                let mut route = vec![];
+                let mut pos = to;
+                while pos != from {
+                    if let Some((prev, dir)) = came_from.get(&pos) {
+                        route.push(*dir);
+                        pos = *prev;
+                    }
+                }
+                route.reverse();
+                return Some(
+                    Route {
+                        from,
+                        to,
+                        tile: self.meta_tiles[to.x as usize + to.y as usize * self.width].clone(),
+                        route,
+                    }
+                );
+            }
+
+            let neighbors = [
+                (Direction::Up, Point8 { x: current.position.x, y: current.position.y.saturating_sub(1) }),
+                (Direction::Down, Point8 { x: current.position.x, y: current.position.y + 1 }),
+                (Direction::Left, Point8 { x: current.position.x.saturating_sub(1), y: current.position.y }),
+                (Direction::Right, Point8 { x: current.position.x + 1, y: current.position.y }),
+            ];
+
+            for (dir, neighbor) in neighbors {
+                if neighbor.x as usize >= self.width || neighbor.y as usize >= self.height {
+                    continue;
+                }
+
+                let tile = &self.meta_tiles[neighbor.x as usize + neighbor.y as usize * self.width];
+                if matches!(tile, MetaTile::Obstacle | MetaTile::Sprite(_)) && neighbor != to {
+                    continue;
+                }
+
+                let tentative_g = g_score.get(&current.position).unwrap_or(&u32::MAX) + 1;
+                if tentative_g < *g_score.get(&neighbor).unwrap_or(&u32::MAX) {
+                    came_from.insert(neighbor, (current.position, dir));
+                    g_score.insert(neighbor, tentative_g);
+                    open_set.push(Node {
+                        position: neighbor,
+                        cost: tentative_g,
+                        heuristic: heuristic(neighbor),
+                    });
+                }
+            }
+        }
+        None
+    }
+
+    pub fn routes(&self, from: Point8) -> Vec<Route> {
+        // 1. routes to warps
+        let mut routes = vec![];
+        for to_map in &self.warp_targets {
+            let target_tile = MetaTile::Warp(*to_map);
+            let shortest_route = self.meta_tiles
+                .iter()
+                .enumerate()
+                .filter(|(_, tile)| tile == &&target_tile)
+                .map(|(index, _)| Point8 { x: (index % self.width) as u8, y: (index / self.width) as u8 })
+                .filter_map(|to| self.route_between(from, to))
+                .min_by(|a, b| a.route.len().cmp(&b.route.len()));
+
+            if shortest_route.is_none() {
+                continue;
+            }
+
+            let mut shortest_route = shortest_route.unwrap();
+
+            if shortest_route.to.x == 0 {
+                shortest_route.route.push(Direction::Left);
+            } else if shortest_route.to.x == (self.width - 1) as u8 {
+                shortest_route.route.push(Direction::Right);
+            } else if shortest_route.to.y == 0 {
+                shortest_route.route.push(Direction::Up);
+            } else if shortest_route.to.y == (self.height - 1) as u8 {
+                shortest_route.route.push(Direction::Down);
+            }
+
+            routes.push(shortest_route);
+        }
+
+        // 2. routes to sprites - keep shortest route per sprite
+        for sprite in &self.sprites {
+            if sprite.hidden {
+                continue;
+            }
+
+            let sprite_pos = sprite.position;
+            let adjacent_positions = [
+                (Direction::Down, Point8 { x: sprite_pos.x, y: sprite_pos.y.saturating_sub(1) }),
+                (Direction::Up, Point8 { x: sprite_pos.x, y: sprite_pos.y + 1 }),
+                (Direction::Right, Point8 { x: sprite_pos.x.saturating_sub(1), y: sprite_pos.y }),
+                (Direction::Left, Point8 { x: sprite_pos.x + 1, y: sprite_pos.y }),
+            ];
+            let shortest_route = adjacent_positions
+                .into_iter()
+                .filter(|(_, pos)| {
+                    pos.x < self.width as u8 && pos.y < self.height as u8 &&
+                        matches!(self.meta_tiles[pos.x as usize + pos.y as usize * self.width], MetaTile::Empty)
+                })
+                .filter_map(|(dir, to)| {
+                    self.route_between(from, to).map(|route| (dir, route))
+                })
+                .min_by(|(_, a), (_, b)| a.route.len().cmp(&b.route.len()));
+
+            if shortest_route.is_none() {
+                continue;
+            }
+            let (sprite_direction, mut shortest_route) = shortest_route.unwrap();
+            shortest_route.tile = MetaTile::Sprite(*sprite);
+            if shortest_route.route.is_empty() || shortest_route.route.last().unwrap() != &sprite_direction {
+                shortest_route.route.push(sprite_direction);
+            }
+            routes.push(shortest_route);
+        }
+        routes
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Route {
+    pub from: Point8,
+    pub to: Point8,
+    pub tile: MetaTile,
+    pub route: Vec<Direction>,
+}
+
+impl Display for MetaTileMap {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let navigable_map = self.navigable_map();
-        let width = self.map_width_blocks * Self::TILES_PER_POSITION;
-        let height = self.map_height_blocks * Self::TILES_PER_POSITION;
-        for y in 0..height {
-            for x in 0..width {
-                if navigable_map[x + y * width] {
-                    write!(f, "#")?;
-                } else {
-                    write!(f, " ")?;
+        for y in 0..self.height {
+            for x in 0..self.width {
+                match self.meta_tiles[x + y * self.width] {
+                    MetaTile::Empty => write!(f, " ")?,
+                    MetaTile::Obstacle => write!(f, "O")?,
+                    MetaTile::Sprite(_) => write!(f, "S")?,
+                    MetaTile::Warp(_) => write!(f, "W")?,
                 }
             }
             writeln!(f)?;
